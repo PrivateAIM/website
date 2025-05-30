b9362c244f1372150d718e4bef9231ff83220198
tada5hi <peter.placzek1996@gmail.com>
2025-05-30 11:59:36 +0200

    chore: add base path to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  assetsInclude: ['**/*.md'],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    /**
+     * todo: remove when moving to apex domain.
+     */
+    base: '/website/',
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    assetsInclude: ['**/*.md'],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
